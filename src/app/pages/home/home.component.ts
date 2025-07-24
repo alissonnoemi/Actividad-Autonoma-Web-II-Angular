5fbf88c0489e9eeb51856c43f326da80c651aea0
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-07-24 10:23:54 -0500

    banner

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { InicioBannerComponent } from "../../components/inicio-banner/inicio-banner.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [InicioBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
