83e4fd3f135cb2d6d1ae79a9550d926f18513132
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-07-25 08:11:16 -0500

    diseño casi mejorado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,32 @@
 module.exports = {
   darkMode: 'class',
-
-};
+  content: [
+    './src/**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'rosa-primario': '#e01e7d',
+        'rosa-borde': '#ff8ecf',
+        'rosa-fondo': '#ffe7f0',
+        'rosa-gradiente': '#ffd6e6',
+        'rosa-card': '#ffc8dd',
+        'rosa-card2': '#ffb6d5',
+        'rosa-texto': '#d48c9c',
+        'amarillo-estrella': '#ffd700',
+        'rosa-nombre': '#dd4a91',
+        'pastel-bg-light': '#FFFBFD',
+        'pastel-bg-dark': '#FFF8FC',
+        'pastel-text-dark': '#8A6A7D',
+        'pastel-text-medium': '#9C8894',
+        'pastel-gradient-start': '#F0C9DA',
+        'pastel-gradient-end': '#E2A9C5',
+        'pastel-border-light': '#F3E2ED',
+        'pastel-button-start': '#ECAED5',
+        'pastel-button-end': '#D48CBF',
+        'pastel-accent-light': '#FAE9F1',
+      },
+    },
+  },
+  plugins: [],
+}
