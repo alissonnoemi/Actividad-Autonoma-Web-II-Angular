fdb679618745817e42b0a2869032449d6a1e0c6a
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-08-06 10:29:32 -0500

    error de destacado, borrado

--- src/app/pages/lista-productos/lista-productos.component.ts
+++ src/app/pages/lista-productos/lista-productos.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductosComponent } from "../../components/productos/productos.component";
 import { BeneficiosComponent } from '../../components/beneficios/beneficios.component';
-import { DestacadoComponent } from '../../components/destacado/destacado.component';
 import { OpinionesComponent } from '../../components/opiniones/opiniones.component';
 
 @Component({
   selector: 'app-lista-productos',
   standalone: true,
-  imports: [ProductosComponent, BeneficiosComponent, DestacadoComponent, OpinionesComponent],
+  imports: [ProductosComponent, BeneficiosComponent, OpinionesComponent],
   templateUrl: './lista-productos.component.html',
   styleUrl: './lista-productos.component.css'
 })
